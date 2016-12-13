06bdd39d41263f91b93f2ce6ba1082d6be1ae932
Jin <1272932541@qq.com>
2016-12-13 10:39:34 +0800

    socket 地址变更

--- client.js
+++ client.js
@@ -98,7 +98,7 @@
 			
 			//连接websocket后端服务器
 			//this.socket = io.connect('ws://realtime.plhwin.com');
-			this.socket = io.connect('ws://localhost:3001/');
+			this.socket = io.connect('ws://www.wyhuan.com:3001');
 			
 			//告诉服务器端有用户登录
 			this.socket.emit('login', {userid:this.userid, username:this.username});
